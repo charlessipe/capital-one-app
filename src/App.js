049414d94d949c7b7017ba3684de269e25f09baa
Charles Sipe <csipe84@gmail.com>
2018-03-24 09:26:08 -0700

    add intro

--- src/App.js
+++ src/App.js
@@ -10,23 +10,21 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Capital One App</h1>
+          <h1 className="App-title">Coffee for a Cause</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Coffee for a Cause Automatically Transfers $5 per day to your account for each day that you skip buying coffee at a coffee shop.
         </p>
 
         <h2>Choose a Charity</h2>
         <Charities className="float-left" charitiesName="Operation Code"/><Charities className="float-left" charitiesName="Seattle Childrens"/>
 
         <button type="button" className="btn btn-primary">Start Donating</button>
-        <p>Amount to Donate Each Day</p>
-        <p>$5</p>
-        <p>$1</p>
-        <p>Other</p>
+        
 
-        <h2>You have donated for X days</h2>
-        <h2>You have donated a total of X to XCharity</h2>
+        <div class="alert alert-success" role="alert">You have donated for <span><strong>X</strong></span> days</div>
+        <div class="alert alert-success" role="info"><h2>You have donated a total of <span>X</span> to XCharity</h2></div>
+
 
       </div>
     );
