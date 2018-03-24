c3ebeb5da8cda998ce13aabf48e5d963c96de84c
Charles Sipe <csipe84@gmail.com>
2018-03-24 13:49:59 -0700

    hardcode charities

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import React, { Component } from 'react';
+import $ from 'jquery';
 import logo from './logo.svg';
 import './App.css';
 import Quiz from './Quiz.jsx';
 import Charities from './Charities/Charities';
 
+
+
 class App extends Component {
+
+  constructor(props){
+    super(props);
+
+    //this.addNote = this.addNote.bind(this);
+
+    this.state = {
+      charities: ["School for Kids Who Can't Read Good and Want to Do Other Stuff Good Too", "Childrens Hospital","Operation Code"]
+    }
+  }
+
+  componentDidMount() {
+
+    fetch('https://api.data.charitynavigator.org/v2/Organizations?app_id=d5e3f2cf&app_key=088acd946a28926a3c1da010e845be92&rated=true&state=WA&zip=98103&sort=RATING')
+      .then(response => response.json()
+      )
+
+    }
+
+
+
   render() {
     return (
       <div className="App">
@@ -18,8 +42,7 @@ class App extends Component {
 
         <div class="container-fluid">
             <div class="col-xs-6">
-              <h2>Sign Up</h2>
-              <p>sign up form here</p>
+              <h2>Sign Up for a Checking Account</h2>
 
               <form>
                 <label for="firstName">First Name</label>
@@ -43,13 +66,16 @@ class App extends Component {
               <div class="form-group">
                 <label for="exampleSelect2">Example multiple select</label>
                 <select multiple="" class="form-control" id="exampleSelect2">
+                  <option>ADA</option>
+                  <option>TAF</option>
+                  <option>School for Kids Who Cant Read Good and Want to Do Other Stuff Good Too</option>
                   <option>Operation Code</option>
-                  <option>Seattle Childrens</option>
-                  <option>3</option>
-                  <option>4</option>
-                  <option>5</option>
                 </select>
               </div>
+
+
+              
+
 
 
               <button type="button" className="btn btn-primary">Start Donating</button>
